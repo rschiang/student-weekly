11397d6ab65d9900f22359b38e2f0520ebe4b6a0
Poren Chiang <ren.chiang@gmail.com>
2016-03-25 22:08:59 +0800

    Add provider def

--- issues/models.py
+++ issues/models.py
@@ -9,6 +9,7 @@ class Issue(models.Model):
     def __str__(self):
         return '<Issue #{}>'.format(self.id)
 
+
 class Column(models.Model):
     # Fields
     name = models.CharField(max_length=32, help_text='專欄名稱')
@@ -21,12 +22,30 @@ class Column(models.Model):
     def __str__(self):
         return '<Column {}{}({})>'.format(self.name, self.remarks, self.id)
 
+
+class Provider(models.Model):
+    # Fields
+    name = models.CharField(max_length=32, help_text='提供者名稱')
+    description = models.TextField(blank=True, help_text='提供者說明')
+    image = models.ImageField(upload_to='providers/', help_text='提供者 Logo')
+    url = models.URLField(blank=True, max_length=256, help_text='提供者網址')
+
+    # Methods
+    def __str__(self):
+        return '<Provider {}({})>'.format(self.name, self.id)
+
+
 class Article(models.Model):
     # Fields
     name = models.CharField(max_length=64, help_text='文章名稱')
     description = models.TextField(blank=True, help_text='文章內容')
     image = models.ImageField(upload_to='%Y/%m/', blank=True, max_length=128, help_text='文章影像')
     url = models.URLField(blank=True, max_length=256, help_text='文章連結網址')
+
+    # Related fields
+    issue = models.ForeignKey(Issue, on_delete=models.CASCADE)
+    column = models.ForeignKey(Column, on_delete=models.PROTECT)
+    provider = models.ForeignKey(Provider, null=True, on_delete=models.SET_NULL)
 
     # Methods
     def __str__(self):
