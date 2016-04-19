78746957fc2d79644ec7900572954998450f3b25
Poren Chiang <ren.chiang@gmail.com>
2016-04-20 05:07:41 +0800

    Simplify login error message

--- ntusa/forms.py
+++ ntusa/forms.py
@@ -3,5 +3,10 @@ from django.contrib.auth.forms import AuthenticationForm
 class HintedAuthenticationForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super(HintedAuthenticationForm, self).__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs['placeholder'] = '使用者名稱'
-        self.fields['password'].widget.attrs['placeholder'] = '密碼'
+        username_widget = self.fields['username'].widget
+        username_widget.attrs['placeholder'] = '使用者名稱'
+        username_widget.attrs['required'] = True
+
+        password_widget = self.fields['password'].widget
+        password_widget.attrs['placeholder'] = '密碼'
+        password_widget.attrs['required'] = True
