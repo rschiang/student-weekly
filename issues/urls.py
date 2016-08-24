179aff313e885e2f1748fae7c7cc33a603ffb74d
Poren Chiang <ren.chiang@gmail.com>
2016-08-25 03:34:32 +0800

    Add placeholder in case of forgotten

--- issues/urls.py
+++ issues/urls.py
@@ -4,4 +4,6 @@ from .views import IssueList
 app_name = 'issues'
 urlpatterns = [
     url(r'^$', IssueList.as_view(), name='list'),
+    #url(r'^([0-9]+)/$', IssueView.as_view(), name='view'),
+    #url(r'^([0-9]+)/edit/$', IssueEdit.as_view(), name='edit'),
 ]
