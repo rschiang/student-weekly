f4500fa9e09a5a419d5ede35e6603965f82a50c6
Poren Chiang <ren.chiang@gmail.com>
2016-09-13 03:30:51 +0800

    Add create issue dialog

--- issues/views.py
+++ issues/views.py
@@ -1,14 +1,26 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import Http404, HttpResponse
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormMixin
 from django.views.generic.detail import SingleObjectMixin
 from django.utils.timezone import now
-from .models import Issue
+from issues.models import Issue
+from templates.models import Template
 
-class IssueList(LoginRequiredMixin, ListView):
+class IssueList(LoginRequiredMixin, FormMixin, ListView):
     queryset = Issue.objects.order_by('-id')
     context_object_name = 'issue_list'
     template_name = 'issues.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['template_list'] = Template.objects.all()
+        context['next_issue_id'] = (context['issue_list'][0].id + 1) if context['issue_list'] else 1
+        context['now'] = now()
+        return context
+
+    def post(self, request):
+        pass    # TODO: Create new issue
 
 class IssueView(SingleObjectMixin, View):
     model = Issue
