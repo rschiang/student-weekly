03b797de2bc894b2168e6178cfcbfe83fff476ce
Poren Chiang <ren.chiang@gmail.com>
2017-03-26 01:35:35 +0800

    Use custom lazy groupby filter

--- ntusa/jinja2.py
+++ ntusa/jinja2.py
@@ -1,3 +1,4 @@
+import itertools
 import jinja2
 from django.conf import settings
 from django.contrib.staticfiles import storage
@@ -7,6 +8,9 @@ from django.utils.timezone import now
 def __url(viewname, *args, **kwargs):
     return urlresolvers.reverse(viewname, args=args, kwargs=kwargs)
 
+def __lazy_groupby(iterable, key_name):
+    return itertools.groupby(iterable, lambda i: i.__dict__[key_name])
+
 def environment(**options):
     env = jinja2.Environment(**options)
     env.globals.update({
@@ -14,5 +18,8 @@ def environment(**options):
         'now': now,
         'static': storage.staticfiles_storage.url,
         'url': __url,
+    })
+    env.filters.update({
+        'lazy_groupby': __lazy_groupby,
     })
     return env
